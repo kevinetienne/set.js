e4aa8401fcd9b679749a7a1684b812fd6a8bb7e1
Kevinwk <kevin@wildknowledge.co.uk>
2011-12-01 11:50:49 +0000

    added first test

--- test/set.test.js
+++ test/set.test.js
@@ -1,3 +1,16 @@
-var assert = require("assert"),
+var vows = require("vows"),
+    assert = require("assert"),
     set = require("../lib/set.js");
+
+var my_set = set.set;
+
+vows.describe('Set').addBatch({
+    'set creation': {
+        topic: Object.create(my_set),
+
+        'is Empty': function(topic) {
+            assert.equal(topic.length, 0);
+        }
+    }
+}).run();
 
